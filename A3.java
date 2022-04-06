ff5773d7208836333c506326340a153d82e9a579
iguhgbril <103067787+iguhgbril@users.noreply.github.com>
2022-04-06 14:18:26 -0300

    Update and rename a3.java to A3.java

--- A3.java
+++ A3.java
@@ -2,7 +2,7 @@ package listaA;
 
 import java.util.Scanner;
 
-public class a3 {
+public class A3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -10,10 +10,11 @@ public class a3 {
 		Scanner meuScanner = new Scanner(System.in);
 		
 	int C, F;
-	System.out.println("digite a temperatura em C°");
+	
+	System.out.println("digite a temperatura em CÂ°");
 	C= meuScanner.nextInt();
 	F = (9 * C + 160 / 5);
-	System.out.println("a temperatura em fahrenheit é" + F);
+	System.out.println("a temperatura em fahrenheit Ã©" + F);
 	
 	}
 
